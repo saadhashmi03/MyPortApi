7201608418120f8622ef95ccef77c8242ae153dc
saadhashmi03 <116589433+saadhashmi03@users.noreply.github.com>
2024-06-08 21:04:48 +0530

    first commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const { connectDB } = require("./connection");
 const routes = require("./routes/routes");
 const cookieParser = require("cookie-parser");
-const path=require('path')
+
 require("dotenv").config();
 
 // connection to db
@@ -22,9 +22,3 @@ app.use("/api", routes);
 // listening to server
 app.listen(port, () => console.log(`server is running on ${port}`));
 
-
-app.get("*",(req,res)=>{
-
-    app.use(express.static(path.resolve(__dirname, "Frontend","dist")))
-    res.sendFile(path.resolve(__dirname,"Frontend","dist","index.html"))
-});
