47c7a424d114d989036d6aa7177e8e7203958f12
saadhashmi03 <116589433+saadhashmi03@users.noreply.github.com>
2024-06-10 03:00:02 +0530

    done

--- index.js
+++ index.js
@@ -11,10 +11,27 @@ require("dotenv").config();
 // connection to db
 connectDB();
 
-// middlewares
-app.use(cors({ origin: "https://my-port-frontend.vercel.app/", credentials: true }));
+// allowed origins
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://my-port-frontend.vercel.app",
+  "https://my-port-frontend-8ozpswg1n-saadhashmi03s-projects.vercel.app"
+];
 
-  
+// CORS options
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+};
+
+// middlewares
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 
@@ -23,4 +40,3 @@ app.use("/api", routes);
 
 // listening to server
 app.listen(port, () => console.log(`server is running on ${port}`));
-
